ed457ea1e74c221aadbfe0fd369848c8a1572263
saiful00000 <saiful161003@gmail.com>
2021-12-13 00:57:17 +0600

    token expiry

--- app/oauth2.py
+++ app/oauth2.py
@@ -2,6 +2,7 @@ from fastapi.security.oauth2 import OAuth2PasswordBearer
 from fastapi import Depends, HTTPException, status
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from typing import Optional
 
 from app.constants import strings
 from . import schemas
@@ -18,16 +19,23 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 # generate and return a JWT token
-def create_access_token(data: dict):
+# def create_access_token(data: dict):
+#     to_encode = data.copy()
+#     expire = datetime.utcnow() + timedelta(seconds=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+#     to_encode.update({"expd": str(expire)})
+
+#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+#     return encoded_jwt
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
-    expire_date = str(
-        datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    )
-
-    to_encode.update({"expd": expire_date})
-
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
     return encoded_jwt
 
 
--- app/routers/post_router.py
+++ app/routers/post_router.py
@@ -14,6 +14,7 @@ def get_all_posts(
     db: Session = Depends(get_db),
     token_data: schemas.TokenData = Depends(oauth2.get_current_user),
 ):
+    print(f'token data from get all posts {token_data}')
     post_list = db.query(models.Post).all()
     return post_list
 
